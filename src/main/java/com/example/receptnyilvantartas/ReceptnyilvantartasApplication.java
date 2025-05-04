6841b1cb524c9b91ffcf991c463ece827013fd0a
Vlukacs0 <vlukacs30@gmail.com>
2025-05-04 22:48:55 +0200

    Első verzió – receptnyilvántartás rendszer

---
+++ src/main/java/com/example/receptnyilvantartas/ReceptnyilvantartasApplication.java
@@ -0,0 +1,13 @@
+package com.example.receptnyilvantartas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptnyilvantartasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptnyilvantartasApplication.class, args);
+	}
+
+}
