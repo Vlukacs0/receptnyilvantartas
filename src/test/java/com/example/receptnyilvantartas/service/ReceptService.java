a8766833fcc126a154536589e22a4d221766b1af
Vlukacs0 <vlukacs30@gmail.com>
2025-05-06 22:31:48 +0200

    service

---
+++ src/test/java/com/example/receptnyilvantartas/service/ReceptService.java
@@ -0,0 +1,12 @@
+package com.example.receptnyilvantartas.service;
+
+import com.example.receptnyilvantartas.model.Recept;
+
+import java.util.List;
+
+public interface ReceptService {
+    List<Recept> findAll();
+    Recept findById(Long id);
+    Recept save(Recept recept);
+    void deleteById(Long id);
+}
