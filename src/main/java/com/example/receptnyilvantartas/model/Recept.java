3fb864b065a12852d64b8850b070c7de8bfa9c0b
Vlukacs0 <vlukacs30@gmail.com>
2025-05-11 23:55:30 +0200

    changes

--- src/main/java/com/example/receptnyilvantartas/model/Recept.java
+++ src/main/java/com/example/receptnyilvantartas/model/Recept.java
@@ -1,14 +1,50 @@
 package com.example.receptnyilvantartas.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Recept {
+
     private Long id;
     private String nev;
     private String leiras;
+
+    public Recept() {
+    }
+
+    public Recept(Long id, String nev, String leiras) {
+        this.id = id;
+        this.nev = nev;
+        this.leiras = leiras;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public String getLeiras() {
+        return leiras;
+    }
+
+    public void setLeiras(String leiras) {
+        this.leiras = leiras;
+    }
+
+    @Override
+    public String toString() {
+        return "Recept{" +
+                "id=" + id +
+                ", nev='" + nev + '\'' +
+                ", leiras='" + leiras + '\'' +
+                '}';
+    }
 }
